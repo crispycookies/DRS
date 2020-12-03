593a78d5b12edac5b3c116de99a28bbc1ed609f6
Patrick Würflinger <wuerflingerp@gmx.at>
2020-12-03 21:54:04 +0100

    fixed bugs

--- src/helper/json_manager.rs
+++ src/helper/json_manager.rs
@@ -2,21 +2,21 @@ use crate::helper::guid;
 use crate::helper::timestamp;
 use json::{JsonResult, JsonValue};
 
-struct Node{
-    address: String,
-    priority: u8
+pub struct Node{
+    pub address: String,
+    pub priority: u8
 }
 
-struct Message{
-    msg_type: u8,
-    payload: String
+pub struct Message{
+    pub msg_type: u8,
+    pub payload: String
 }
 
 pub struct Protocol {
-    id: String,
-    timestamp: String,
-    node: Node,
-    msg: Message
+    pub id: String,
+    pub timestamp: String,
+    pub node: Node,
+    pub msg: Message
 }
 
 pub fn get_guid() -> String{
@@ -27,11 +27,11 @@ pub fn get_timestamp() -> String{
     return timestamp::get_time_us();
 }
 
-pub fn deserialize(sequence: &str) -> JsonResult<JsonValue>{
-    return json::parse(sequence);
+pub fn deserialize(sequence: String) -> JsonResult<JsonValue>{
+    return json::parse(&sequence);
 }
 
-pub fn serialize() -> String{
-    return guid::create_random_guid();
+pub fn serialize(pro: Protocol) -> String{
+    return json::stringify(pro)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::helper::json_manager::{Node, Protocol, Message};
+
 //mod master;
 //mod slave;
 mod helper;
@@ -5,8 +7,12 @@ mod helper;
 //use std::thread;
 
 fn main() {
+    let n: Node = Node {address: "Hallo".to_string(), priority: 0x3};
+    let m: Message = Message{msg_type: 0x1, payload: "Seas".to_string()};
+    let p: Protocol = Protocol { id:"Seas".to_string(), timestamp:"Seas".to_string(), node: n, msg: m };
+
     println!("{}", helper::json_manager::get_guid());
     println!("{}", helper::json_manager::get_timestamp());
-    println!("{}", helper::json_manager::serialize());
-    helper::json_manager::deserialize("hallo");
+    println!("{}", helper::json_manager::serialize(p));
+    helper::json_manager::deserialize("hallo".to_string());
 }
