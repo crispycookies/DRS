a255e186e65dcc157bb73f2399436a684f5eddfd
tobi <s1910567016@students.fh-hagenberg.at>
2020-12-09 21:27:52 +0100

    Fixed Compiler Error

--- src/comm/comm.rs
+++ src/comm/comm.rs
@@ -1,32 +1,31 @@
 use crate::comm::comm_low_level::Comm;
-use crate::helper::json_manager::{Protocol, serialize, deserialize};
+use crate::helper::json_manager::{Protocol, serialize, deserialize, Node, Message};
 use std::io::Error;
 
 
 struct EasyComm {
-    pub comm : Comm
+    pub comm: Comm
 }
+
 impl EasyComm {
-    pub fn send_package(&self, protocol : Protocol) -> std::io::Result<bool>{
+    pub fn send_package(&self, protocol: Protocol) -> std::io::Result<()> {
         let serialized = serialize(protocol);
+        match self.comm.send(serialized) {
+            Ok(_) => { Ok(()) }
+            Err(e) => { Err(e) }
+        }
+    }
+    pub fn receive_package(&self) -> std::io::Result<Protocol> {
         let mut recvd = "".to_string();
-        match  self.comm.send(serialized) {
-            Ok(_) => {Ok(true)}
-            Err(e) => {Err(e)}
-        }
         match self.comm.receive() {
-            Ok(e) => {recvd = e}
-            Err(_) => {Ok(false)}
-        }
-        match deserialize(recvd) {
             Ok(e) => {
-                if(e.msg.msg_type == 9){
-                    Ok(true)
+                recvd = e;
+                match deserialize(recvd){
+                    Ok(e) => {Ok((e))}
+                    Err(e) => {Err(e)}
                 }
-                Ok(false)
             }
-            Err(e) => {Err(e)}
+            Err(e) => { Err(e) }
         }
-        Ok(false)
     }
 }
