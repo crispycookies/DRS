d04375295c05561f6a57f62aeb8ee9563d721f4f
Tobias Egger <crispycookies@users.noreply.github.com>
2020-12-10 15:38:22 +0100

    Removed Duplicate Time Class

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,4 +1,3 @@
 mod guid;
-mod timestamp;
 pub mod json_manager;
 pub mod time;
--- src/helper/timestamp.rs
+++
@@ -1,6 +0,0 @@
-use std::time::SystemTime;
-
-#[allow(dead_code)]
-pub fn get_time_us() -> String {
-    return SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_micros().to_string();
-}
