274746b6248d9a78a5e1bbac77acadf46474376f
schuchi07 <57154286+schuchi07@users.noreply.github.com>
2021-01-02 20:39:39 +0100

    finished

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod helper;
 mod master_fsm;
 
 use crate::helper::json_manager::{Node, Message, Protocol};
-use rppal::gpio::Gpio;
-use rppal::system::{DeviceInfo};
+//use rppal::gpio::Gpio;
+//use rppal::system::{DeviceInfo};
 use std::env;
 use crate::master_fsm::{Master, MessageTypes, InnerMaster};
 use std::time::Duration;
 use std::collections::HashMap;
+use rand::Rng;
 
 fn slave() -> () {
     let args: Vec<String> = env::args().collect();
@@ -21,60 +22,73 @@ fn slave() -> () {
     guid.create_random_guid();
 
     let time = helper::time::Time { tim_offset: std::sync::Mutex::new(0) };
-    let prio = 0x3;
-
+    // Use the received time offset to correct the local time
+    let mut rng = rand::thread_rng();
+    let prio = rng.gen_range(1..100);
 
     let cloned_guid = guid.guid.clone();
-    let n: Node = Node { address: args.get(1).unwrap().to_string(), priority: prio.clone() };
+    let n: Node = Node { address: slave.comm.own_addr.clone(), priority: prio.clone() };
     let m: Message = Message { msg_type: MessageTypes::LanBroadcast as u8, payload: "".to_string() };
     let p: Protocol = Protocol { id: cloned_guid, timestamp: time.get_time_with_offset().to_string(), node: n, msg: m };
 
-    loop {
-        match slave.send_package(p.clone()) {
-            Ok(_) => {
-                match slave.receive_package() {
-                    Ok(e) => {
-                        if e.msg.msg_type == MessageTypes::LanBroadcastAck as u8 {
-                            print!("Received Ack\n")
+    match slave.send_package(p.clone()) {
+        Ok(_) => {
+            match slave.receive_package() {
+                Ok(e) => {
+                    println!("Received {}", e.msg.msg_type.to_string());
+                    if e.msg.msg_type == MessageTypes::LanBroadcastAck as u8 {
+                        println!("Received Ack\n")
+                    }
+                }
+                Err(_) => {}
+            }
+        }
+        Err(_) => {
+            println!("Failed to send registration package\n");
+            std::thread::sleep(Duration::from_secs(1));
+        }
+    }
+
+    loop{
+        match slave.receive_package() {
+            Ok(e) => {
+                if e.msg.msg_type == MessageTypes::SendTimestamp as u8 {
+                    //println!("Received Timestamp-Request from Master...\n");
+
+                    // Store time when the message was received and send it back to master
+                    // Build protocol package
+                    let mut current_timestamp = time.get_time_with_offset();
+                    let mut ts_response = p.clone();
+                    ts_response.msg.msg_type = MessageTypes::ResponseWithTime as u8;
+                    ts_response.timestamp = current_timestamp.to_string();
+
+                    // Send timestamp back to master
+                    match slave.send_package(ts_response) {
+                        Ok(_) => {}
+                        Err(_) => {
+                            //println!("Failed to send timestamp back to Master\n");
                         }
                     }
-                    Err(_) => {}
+                }
+                else if e.msg.msg_type == MessageTypes::TimeOffset as u8{
+                    //println!("Received TimeOffset-Request from Master...\n");
+
+                    // Use the received time offset to correct the local time
+                    let offset: i128 = e.msg.payload.parse().unwrap();
+                    println!("Offset: {}", offset);
+                    *time.tim_offset.lock().unwrap() = offset;
                 }
             }
             Err(_) => {
-                print!("Failed to send package\n");
-                std::thread::sleep(Duration::from_secs(1));
+                //println!("Failed to receive message...\n");
             }
         }
-
-
-        /* match slave.receive_package() {
-             Ok(e) => {
-                 print!("received package {}", helper::json_manager::serialize(e.clone()));
-                 if e.msg.msg_type == 0x03 {
-                     let cloned_guid = guid.guid.clone();
-                     let n: Node = Node { address: args.get(1).unwrap().to_string(), priority: prio.clone() };
-                     let m: Message = Message { msg_type: 0x04, payload: "".to_string() };
-                     let p: Protocol = Protocol { id: cloned_guid, timestamp: time.get_time_with_offset().to_string(), node: n, msg: m };
-
-                     match slave.send_package(p) {
-                         Ok(_) => { print!("Succeeded in Sending\n") }
-                         Err(_) => { print!("Failed to send\n") }
-                     }
-                 }
-             }
-             Err(e) => {
-                 print!("No Package received or package damaged!\n");
-                 print!("{}", e);
-             }
-         }
-         */
     }
 }
 
 fn run_pin_toggle(time: std::sync::Arc<std::sync::Mutex<helper::time::Time>>, pin: u8, desktop_mode: bool) {
     if desktop_mode {
-        const SEC_DIVIDER: u128 = 1000000;
+        const SEC_DIVIDER: u128 = 6000000;
         const SEC_ADDER: u128 = SEC_DIVIDER / 2;
 
         let mut start_time = time.lock().unwrap().get_time_with_offset();
@@ -87,7 +101,7 @@ fn run_pin_toggle(time: std::sync::Arc<std::sync::Mutex<helper::time::Time>>, pi
             println!("Test {}:{}:s-{}", time.lock().unwrap().get_time_with_offset(), next_scheduled, start_time);
         }
     } else {
-        let mut gpio_pin = Gpio::new().expect("...").get(pin).expect("Wrong Pin").into_output();
+        //let mut gpio_pin = Gpio::new().expect("...").get(pin).expect("Wrong Pin").into_output();
         const SEC_DIVIDER: u128 = 1000000;
         const SEC_ADDER: u128 = SEC_DIVIDER / 2;
         let mut start_time = time.lock().unwrap().get_time_with_offset();
@@ -97,11 +111,11 @@ fn run_pin_toggle(time: std::sync::Arc<std::sync::Mutex<helper::time::Time>>, pi
         loop {
             next_scheduled += SEC_ADDER;
             while time.lock().unwrap().get_time_with_offset() < next_scheduled {}
-            if gpio_pin.is_set_high() {
+            /*if gpio_pin.is_set_high() {
                 gpio_pin.set_low();
             } else {
                 gpio_pin.set_high();
-            }
+            }*/
             println!("Test {}:{}:s-{}", time.lock().unwrap().get_time_with_offset(), next_scheduled, start_time);
         }
     }
@@ -113,14 +127,14 @@ fn main() {
     let time_arc_for_master_run_thread = time_arc.clone();
     let args: Vec<String> = env::args().collect();
 
-    match DeviceInfo::new() {
+    /*match DeviceInfo::new() {
         Ok(e) => {
             println!("Running DRS on {}.", e.model());
         }
         Err(_) => {
             println!("Running on Generic Desktop PC or similar");
         }
-    }
+    }*/
 
     let spawn = std::thread::spawn(move || {
         run_pin_toggle(time_arc_for_thread, 12, true);
@@ -133,11 +147,11 @@ fn main() {
                 communication_if: cf,
                 time: time_arc_for_master_run_thread,
                 prio: 0xFF,
-                client_vector: std::sync::Mutex::new(HashMap::new())
+                client_vector: HashMap::new()
             }
         };
-
-        master.init(args.get(1).expect("expect a foreign address").to_string(),
+    //args.get(1).expect("expect a foreign address").to_string()
+        master.init("".to_string(),
                     args.get(2).expect("expect own address").to_string(),
                     args.get(3).expect("expect a timeout value").
                         parse::<u64>().expect("expect a valid timeout"));
--- src/master_fsm.rs
+++ src/master_fsm.rs
@@ -1,6 +1,7 @@
 use crate::{comm, helper};
 use crate::helper::json_manager::{Node, Message, Protocol};
 use std::collections::HashMap;
+use std::num::ParseIntError;
 
 
 #[allow(dead_code)]
@@ -42,7 +43,7 @@ pub struct InnerMaster {
     pub communication_if: comm::comm::EasyComm,
     pub time: std::sync::Arc<std::sync::Mutex<helper::time::Time>>,
     pub prio: u8,
-    pub client_vector: std::sync::Mutex<HashMap<u8, Client>>,
+    pub client_vector: HashMap<u8, Client>,
 }
 
 
@@ -57,6 +58,7 @@ impl Master {
         let pos = url.find(':');
         url[0..pos.expect("Expected Port; none given")].to_string()
     }
+
     pub fn match_ack(&mut self, p: Protocol, ack_msg_type: MessageTypes) -> Option<Protocol> {
         return match self.inner.communication_if.send_package(p.clone()) {
             Ok(_) => {
@@ -68,7 +70,6 @@ impl Master {
                             None
                         }
                     }
-
                     Err(_) => {
                         None
                     }
@@ -79,6 +80,7 @@ impl Master {
             }
         };
     }
+
     pub fn broadcast_lan(&mut self, guid: String) -> bool {
         let msg_type: MessageTypes = MessageTypes::LanBroadcast;
         let payload = "".to_string();
@@ -120,8 +122,9 @@ impl Master {
             }
         };
     }
+
     pub fn broadcast_offset(&mut self, guid: String) -> bool {
-        let msg_type: MessageTypes = MessageTypes::LanBroadcast;
+        let msg_type: MessageTypes = MessageTypes::TimeOffset;
         let payload = (*self.inner.time.lock().unwrap().tim_offset.lock().unwrap()).to_string();
         let n: Node = Node { address: self.get_url(self.inner.communication_if.comm.own_addr.clone()), priority: self.inner.prio };
         let m: Message = Message { msg_type: msg_type as u8, payload };
@@ -141,6 +144,55 @@ impl Master {
         };
     }
 
+    pub fn send_offset_to_client(&mut self, guid: String, val: i128) {
+        let msg_type: MessageTypes = MessageTypes::TimeOffset;
+        let payload = val.to_string();
+        let n: Node = Node { address: self.get_url(self.inner.communication_if.comm.own_addr.clone()), priority: self.inner.prio };
+        let m: Message = Message { msg_type: msg_type as u8, payload };
+        let p: Protocol = Protocol { id: guid, timestamp: self.inner.time.lock().unwrap().get_time_with_offset().to_string(), node: n, msg: m };
+
+        match self.inner.communication_if.send_package(p) {
+            Ok(_) => {}
+            Err(_) => {}
+        };
+    }
+
+    pub fn get_client_time_stamp(&mut self, guid: String) -> Option<u128> {
+        let msg_type: MessageTypes = MessageTypes::SendTimestamp;
+        let payload = "".to_string();
+        let n: Node = Node { address: self.get_url(self.inner.communication_if.comm.own_addr.clone()), priority: self.inner.prio };
+        let m: Message = Message { msg_type: msg_type as u8, payload };
+        let p: Protocol = Protocol { id: guid, timestamp: self.inner.time.lock().unwrap().get_time_with_offset().to_string(), node: n, msg: m };
+
+        match self.inner.communication_if.send_package(p) {
+            Ok(_) => {
+                match self.inner.communication_if.receive_package() {
+                    Ok(e) => {
+                        if e.msg.msg_type == MessageTypes::ResponseWithTime as u8 {
+                            match e.timestamp.parse::<u128>() {
+                                Ok(e) => {
+                                    return Some(e)
+                                }
+                                Err(_) => {
+                                    return None
+                                }
+                            }
+                        } else {
+                            return None
+                        }
+                    }
+
+                    Err(_) => {
+                        return None
+                    }
+                }
+            }
+            Err(_) => {
+                return None
+            }
+        };
+    }
+
     #[allow(unused_assignments)]
     pub fn register(&mut self, e: Protocol, guid: String) -> Register {
         if e.msg.msg_type == MessageTypes::LanBroadcast as u8 {
@@ -155,14 +207,14 @@ impl Master {
 
             let f_addr_rec_d = e.node.address.clone();
 
-            let check = self.inner.client_vector.lock().unwrap().contains_key(&e.node.priority);
+            let check = self.inner.client_vector.contains_key(&e.node.priority);
 
             if check {
-                print!("Client with Priority {} is already registered for address {}, dropping address {}\n", e.node.priority, self.inner.client_vector.lock().unwrap().get(&e.node.priority).unwrap().url, e.node.address);
+                print!("Client with Priority {} is already registered for address {}, dropping address {}\n", e.node.priority, self.inner.client_vector.get(&e.node.priority).unwrap().url, e.node.address.clone());
                 r_val = Register::NoneRegistered;
             } else {
-                print!("Client with Priority {} is already registered for address {}, dropping address {}\n", e.node.priority, self.inner.client_vector.lock().unwrap().get(&e.node.priority).unwrap().url, e.node.address);
-                self.inner.client_vector.lock().unwrap().insert(e.node.priority, Client { url: e.node.address });
+                self.inner.client_vector.insert(e.node.priority, Client { url: e.node.address.clone() });
+                print!("Client with Priority {} is registering for address {}", e.node.priority, self.inner.client_vector.get(&e.node.priority).unwrap().url);
                 r_val = Register::OneRegistered;
             }
 
@@ -178,19 +230,50 @@ impl Master {
         return Register::NoneReceived;
     }
 
-
-
     pub fn run_cyclic(&mut self, guid: String) {
         loop {
-            let guid_clone = guid.clone();
-            match self.inner.communication_if.receive_package() {
-                Ok(e) => {
-                    if self.register(e.clone(), guid_clone.clone()) as u8 != Register::NoneReceived as u8 {}
+            if(self.inner.client_vector.len() != 2) {
+                match self.inner.communication_if.receive_package() {
+                    Ok(e) => {
+                        if self.register(e.clone(), guid.clone()) as u8 != Register::NoneReceived as u8 {}
+                    }
+
+                    Err(_) => {}
+                }
+            }
+
+            let copy = self.inner.client_vector.clone();
+
+            for (k,v) in copy {
+
+                let mut times:Vec<i128> = Vec::new();
+                self.inner.communication_if.comm.foreign_addr = v.url.clone();
+
+                for n in 0..3 {
+                    let t1 = self.inner.time.lock().unwrap().get_time_with_offset();
+                    let t2 = self.get_client_time_stamp(guid.clone());
+                    if (t2.is_some()) {
+                        let t = t2.unwrap() as i128 - t1 as i128;
+                        times.push(t);
+                    }
                 }
 
-                Err(_) => {}
+                let mut maxValue:i128 = 0;
+                match times.iter().max() {
+                    Some(e) => {maxValue = *e}
+                    None => {maxValue = times[2]}
+                }
+
+                let index = times.iter().position(|x| *x == maxValue).unwrap();
+                times.remove(index);
+
+                let offset_time = (times[0] + times[1]) / 2;
+                self.send_offset_to_client(guid.clone(), offset_time);
             }
         }
+    }
+    pub fn run_sync(&mut self, guid: String){
+
     }
 
     pub fn run(&mut self){
@@ -206,20 +289,22 @@ impl Master {
         self.inner.communication_if.init(timeout.clone(), true);
 
 
-        self.inner.client_vector.lock().unwrap().insert(self.inner.prio, Client { url: o_addr });
+        //self.inner.client_vector.insert(self.inner.prio, Client { url: o_addr });
 
 
         let mut guid = helper::guid::RandomGuid { guid: "".to_string() };
         guid.create_random_guid();
 
+        let guid_copy = guid.guid.clone();
+
         match crossbeam::scope(|scope| {
             scope.spawn(move |_| {
-                self.run();
+                self.run_cyclic(guid_copy.clone());
+                self.run_sync(guid_copy.clone());
             });
         }) {
             Ok(_) => {}
             Err(_) => {}
         }
-
     }
 }
